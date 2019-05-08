78d158f1403dae4baf80384c4f1147472b35a640
Anvitha-Jain <anvithjain@gmail.com>
2019-05-08 07:32:37 -0700

    Update Config.java

--- AR_Store_Shop/app/src/main/java/com/example/ar_store_shop/config/Config.java
+++ AR_Store_Shop/app/src/main/java/com/example/ar_store_shop/config/Config.java
@@ -2,6 +2,5 @@ package com.example.ar_store_shop.config;
 
 public class Config {
 
-    public static final String PAYPAL_CLIENT_ID = "ATJMsdoL1leUPSteoRNQ3wjg1LDcC90idPFc7Gta2tAmdboN1s3QexWcfmuZTmjwikXeTKr53ZW4WOU3";
-
+    public static final String PAYPAL_CLIENT_ID = ""
 }
